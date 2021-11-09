eef8852b61ef961ed3c39732f86f4f9ca954ffda
Andrea <a.dilorenzo@dema-engineering.it>
2021-11-09 14:15:22 +0100

    real-time component

--- src/app/components/real-time/real-time.module.ts
+++ src/app/components/real-time/real-time.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 
 import { RealTimeRoutingModule } from './real-time.routing';
 import { RealTimeComponent } from './real-time.component';
+import { NptMapModule } from '@npt/npt-map';
+import { MaterialModule } from '@npt/npt-template';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 
 @NgModule({
@@ -11,6 +14,9 @@ import { RealTimeComponent } from './real-time.component';
   ],
   imports: [
     CommonModule,
+    NptMapModule,
+    MaterialModule,
+    FlexLayoutModule,
     RealTimeRoutingModule
   ]
 })
