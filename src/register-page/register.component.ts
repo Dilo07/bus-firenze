b22fb1b4ffa16827e5e8fb796cbbf784592b877a
Andrea <a.dilorenzo@dema-engineering.it>
2021-11-12 12:59:36 +0100

    angular in register

---
+++ src/register-page/register.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.html',
+  styles: [``]
+})
+export class RegisterComponent implements OnInit {
+  public angular = true;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
