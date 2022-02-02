7f7ba9c4e3d6ed17cb0124b737faf4fbfb0b5b8f
Andrea <a.dilorenzo@dema-engineering.it>
2022-02-02 15:26:07 +0100

    remove unusued dep

--- src/app/components/comp-manage-obu/manage-obu.module.ts
+++ src/app/components/comp-manage-obu/manage-obu.module.ts
@@ -1,10 +1,5 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { TranslateModule } from '@ngx-translate/core';
-import { MaterialModule } from '@npt/npt-template';
-import { ZXingScannerModule } from '@zxing/ngx-scanner';
 import { ManageObuRoutingModule } from './manage-obu-routing';
 
 @NgModule({
