4b11e29ebb40aeb58b245a804bf226da9696fe08
Andrea <a.dilorenzo@dema-engineering.it>
2022-02-02 15:05:42 +0100

    rimossi appuntamenti e manage obu - importata lib npt-obu

--- src/app/components/comp-manage-obu/manage-obu.module.ts
+++ src/app/components/comp-manage-obu/manage-obu.module.ts
@@ -6,30 +6,11 @@ import { TranslateModule } from '@ngx-translate/core';
 import { MaterialModule } from '@npt/npt-template';
 import { ZXingScannerModule } from '@zxing/ngx-scanner';
 import { ManageObuRoutingModule } from './manage-obu-routing';
-import { ModalObuComponent } from './modal-obu/modal-obu.component';
-import { ObuComponent } from './obu.component';
-import { ModalPlateComponent } from './modal-plate/modal-plate.component';
-import { VehicleDocumentComponent } from './vehicle-document/vehicle-document.component';
-import { PdfViewerModule } from 'ng2-pdf-viewer';
-
 
 @NgModule({
-  declarations: [
-    ObuComponent,
-    ModalObuComponent,
-    ModalPlateComponent,
-    VehicleDocumentComponent
-  ],
   imports: [
     CommonModule,
-    MaterialModule,
-    FlexLayoutModule,
-    TranslateModule,
-    FormsModule,
-    ReactiveFormsModule,
-    ManageObuRoutingModule,
-    ZXingScannerModule,
-    PdfViewerModule
+    ManageObuRoutingModule
   ]
 })
 export class ManageObuModule { }
