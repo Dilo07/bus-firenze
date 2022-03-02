d9db5442514d7552e1ac43297aa6b9a150e70dbf
Andrea <a.dilorenzo@dema-engineering.it>
2022-03-02 10:22:39 +0100

    check fiscal code

--- src/app/components/domain/bus-firenze-constants.ts
+++ src/app/components/domain/bus-firenze-constants.ts
@@ -81,7 +81,6 @@ export const TICKETS_TYPE = {
 };
 
 export const FLEETMNG_TYPE = Object.freeze({
-    PERSONA_FISICA: 'PERS',
     AZIENDA_PRIVATA: 'PRVT',
     PUBBLICA_AMM: 'PA',
     ENTE: 'ENTE'
