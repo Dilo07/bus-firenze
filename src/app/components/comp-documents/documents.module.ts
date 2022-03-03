e4e52bd63d9e7645519429ed76736826a192442b
Andrea <a.dilorenzo@dema-engineering.it>
2022-03-03 08:52:25 +0100

    comp document

--- src/app/components/comp-documents/documents.module.ts
+++ src/app/components/comp-documents/documents.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 
 import { DocumentsRoutingModule } from './documents-routing.module';
 import { DocumentsComponent } from './documents/documents.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModule } from '@npt/npt-template';
+import { TranslateModule } from '@ngx-translate/core';
 
 
 @NgModule({
@@ -11,6 +14,9 @@ import { DocumentsComponent } from './documents/documents.component';
   ],
   imports: [
     CommonModule,
+    MaterialModule,
+    FlexLayoutModule,
+    TranslateModule,
     DocumentsRoutingModule
   ]
 })
