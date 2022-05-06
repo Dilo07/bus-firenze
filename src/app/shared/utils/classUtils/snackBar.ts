ad9eeabf645090eae40bf1632c8872da84be8d56
Andrea <a.dilorenzo@dema-engineering.it>
2022-05-06 09:36:44 +0200

    import new lib

--- src/app/shared/utils/classUtils/snackBar.ts
+++ src/app/shared/utils/classUtils/snackBar.ts
@@ -3,21 +3,21 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { TranslateService } from '@ngx-translate/core';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class SnackBar {
 
-    constructor(private snackBar: MatSnackBar, private translate: TranslateService) { }
+  constructor(private snackBar: MatSnackBar, private translate: TranslateService) { }
 
-    public showMessage(i18nKey: string, level: string): void {
+  public showMessage(i18nKey: string, level: string): void {
 
-        this.snackBar.open(this.translate.instant(i18nKey),
-            '✖',
-            {
-                duration: 3000,
-                horizontalPosition: 'center',
-                verticalPosition: 'top',
-                panelClass: [level]
-            });
-    }
+    this.snackBar.open(this.translate.instant(i18nKey),
+      '✖',
+      {
+        duration: 3000,
+        horizontalPosition: 'center',
+        verticalPosition: 'top',
+        panelClass: [level]
+      });
+  }
 }
