a8cb3a052b109a3bfbcdad90b7fd5455deb284b0
Andrea <a.dilorenzo@dema-engineering.it>
2022-05-09 16:08:04 +0200

    reorder menu

--- src/app/npt-template-menu/menu-item.service.ts
+++ src/app/npt-template-menu/menu-item.service.ts
@@ -14,6 +14,7 @@ export const ROLES = Object.freeze({
 const SUBMENU_ROUTES = [
   { state: 'fleet-manager-manage', name: 'Fleet-manager', icon: 'manage_accounts', roles: [ROLES.MOVYON, ROLES.OPER_MOVYON] },
   { state: 'deposit', name: 'Deposit', icon: 'euro_symbol', roles: [ROLES.MOVYON, ROLES.OPER_MOVYON, ROLES.FLEETMNG] },
+  { state: 'billing', name: 'Billing', icon: 'euro_symbol', roles: [ROLES.MOVYON, ROLES.OPER_MOVYON, ROLES.FLEETMNG] },
   { state: 'fleet-manager-valid', name: 'Valid-Fleet-manager', icon: 'manage_accounts', roles: [ROLES.MOVYON, ROLES.OPER_MOVYON] },
   { state: 'vehicle-valid', name: 'Vehicle-valid', icon: 'directions_car', roles: [ROLES.MOVYON, ROLES.OPER_MOVYON] },
   { state: 'vehicles', name: 'Vehicles', icon: 'directions_car', roles: [ROLES.FLEETMNG] },
@@ -29,8 +30,7 @@ const SUBMENU_ROUTES = [
   { state: 'anagraphic-driver', name: 'Anagraphic', icon: 'manage_accounts', roles: [ROLES.DRIVER] },
   { state: 'association-driver', name: 'Association-vehicles', icon: 'directions_car', roles: [ROLES.DRIVER] },
   { state: 'add-ticket', name: 'Add-ticket', icon: 'receipt_long', roles: [ROLES.MOVYON, ROLES.FLEETMNG, ROLES.DRIVER] },
-  { state: 'manage-ticket', name: 'Manage-ticket', icon: 'receipt_long', roles: [ROLES.MOVYON, ROLES.FLEETMNG, ROLES.DRIVER] },
-  { state: 'billing', name: 'Billing', icon: 'euro_symbol', roles: [ROLES.MOVYON, ROLES.OPER_MOVYON, ROLES.FLEETMNG] },
+  { state: 'manage-ticket', name: 'Manage-ticket', icon: 'receipt_long', roles: [ROLES.MOVYON, ROLES.FLEETMNG, ROLES.DRIVER] }
 ];
 
 const MENUITEMS = [
@@ -50,36 +50,36 @@ const MENUITEMS = [
   },
   {
     state: '', name: 'Manage', type: 'submenu', icon: 'manage_accounts',
-    children: [SUBMENU_ROUTES[0], SUBMENU_ROUTES[1], SUBMENU_ROUTES[18]], roles: [ROLES.MOVYON, ROLES.OPER_MOVYON]
+    children: [SUBMENU_ROUTES[0], SUBMENU_ROUTES[1], SUBMENU_ROUTES[2]], roles: [ROLES.MOVYON, ROLES.OPER_MOVYON]
   },
   {
     state: '', name: 'Validation', type: 'submenu', icon: 'manage_accounts',
-    children: [SUBMENU_ROUTES[2], SUBMENU_ROUTES[3]], roles: [ROLES.MOVYON, ROLES.OPER_MOVYON]
+    children: [SUBMENU_ROUTES[3], SUBMENU_ROUTES[4]], roles: [ROLES.MOVYON, ROLES.OPER_MOVYON]
   },
   {
     state: 'payments', name: 'Payments', type: 'submenu', icon: 'manage_accounts',
-    children: [SUBMENU_ROUTES[1], SUBMENU_ROUTES[18]], roles: [ROLES.FLEETMNG]
+    children: [SUBMENU_ROUTES[1], SUBMENU_ROUTES[2]], roles: [ROLES.FLEETMNG]
   },
   {
     state: 'user-fleet-manager', name: 'User-Fleet', type: 'submenu', icon: 'manage_accounts',
-    children: [SUBMENU_ROUTES[4], SUBMENU_ROUTES[5], SUBMENU_ROUTES[6]], roles: [ROLES.FLEETMNG]
+    children: [SUBMENU_ROUTES[5], SUBMENU_ROUTES[6], SUBMENU_ROUTES[7]], roles: [ROLES.FLEETMNG]
   },
   {
     state: 'manage-obu', name: 'Manage-obu', type: 'submenu', icon: 'assignment',
-    children: [SUBMENU_ROUTES[7], SUBMENU_ROUTES[8], SUBMENU_ROUTES[9], SUBMENU_ROUTES[10], SUBMENU_ROUTES[11]],
+    children: [SUBMENU_ROUTES[8], SUBMENU_ROUTES[9], SUBMENU_ROUTES[10], SUBMENU_ROUTES[11], SUBMENU_ROUTES[12]],
     roles: [ROLES.MOVYON, ROLES.INSTALLER]
   },
   {
     state: 'appointment', name: 'Appointment', type: 'submenu', icon: 'assignment',
-    children: [SUBMENU_ROUTES[12], SUBMENU_ROUTES[13]], roles: [ROLES.MOVYON, ROLES.INSTALLER]
+    children: [SUBMENU_ROUTES[13], SUBMENU_ROUTES[14]], roles: [ROLES.MOVYON, ROLES.INSTALLER]
   },
   {
     state: 'user-driver', name: 'User-Driver', type: 'submenu', icon: 'manage_accounts',
-    children: [SUBMENU_ROUTES[14], SUBMENU_ROUTES[15]], roles: [ROLES.DRIVER]
+    children: [SUBMENU_ROUTES[15], SUBMENU_ROUTES[16]], roles: [ROLES.DRIVER]
   },
   {
     state: 'ticket', name: 'Ticket', type: 'submenu', icon: 'manage_accounts',
-    children: [SUBMENU_ROUTES[16], SUBMENU_ROUTES[17]], roles: [ROLES.MOVYON, ROLES.FLEETMNG, ROLES.DRIVER]
+    children: [SUBMENU_ROUTES[17], SUBMENU_ROUTES[18]], roles: [ROLES.MOVYON, ROLES.FLEETMNG, ROLES.DRIVER]
   },
   {
     state: 'documents', name: 'Documents', type: 'link', icon: 'assignment',
