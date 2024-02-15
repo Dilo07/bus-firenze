1f3b847f50b45a092704ca47d1001ff71755ed80
adilorenzodema <a.dilorenzo@dema-engineering.it>
2024-02-15 11:56:27 +0100

    fix NPT-1153

--- src/app/components/comp-fleet-manager/drivers/details-form-driver/modal-new-driver/modal-new-driver.component.ts
+++ src/app/components/comp-fleet-manager/drivers/details-form-driver/modal-new-driver/modal-new-driver.component.ts
@@ -1,6 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
 import { SnackBar } from '@npt/npt-template';
 import { Driver, FleetManager } from 'src/app/components/domain/bus-firenze-domain';
 import { DriverService } from 'src/app/services/driver.service';
@@ -17,7 +16,6 @@ export class ModalNewDriverComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
     private snackBar: SnackBar,
-    private router: Router,
     private driverService: DriverService,
     public dialogRef: MatDialogRef<ModalNewDriverComponent>,
     @Inject(MAT_DIALOG_DATA) public data: {fleetManager: FleetManager}) { }
@@ -37,7 +35,7 @@ export class ModalNewDriverComponent implements OnInit {
     newDriver.contacts = [];
     const mail = { code: 3, value: this.formGroup.get('ctrlMail').value };
     newDriver.contacts.push(mail);
-    this.driverService.addDriver(newDriver, this.data.fleetManager.id).subscribe({
+    this.driverService.addDriver(newDriver, this.data?.fleetManager?.id).subscribe({
       error: () => this.snackBar.showMessage('DRIVERS.ADD_ERROR', 'ERROR'),
       complete: () => {
         this.snackBar.showMessage('DRIVERS.ADD_SUCCESS', 'INFO');
